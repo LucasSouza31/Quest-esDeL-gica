5a4151d97f7c3b9cb3b59b204f1dffb01dbb9a82
lucassouza31 <lucaseasy10@gmail.com>
2024-08-13 09:33:36 -0300

    Iniciando o projeto

---
+++ exercicios/src/questoes/Q01.kt
@@ -0,0 +1,11 @@
+package questoes
+
+//1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma
+//entre A e B e mostre se a soma é menor que C
+
+fun main() {
+
+}
+
+
+
